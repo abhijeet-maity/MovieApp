aaf553842711c59a049d889f07493d58c9ccba8f
abhijeet-maity <abhijeetmaity1000@gmail.com>
2025-01-19 23:27:10 +0530

    added footer to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import SearchPage from "./pages/SearchPage.jsx";
 import TopRatedPage from "./pages/TopRatedPage.jsx";
 import UpcomingMovie from "./pages/UpcomingMovie.jsx";
 import SingleMovieDetailPage from "./pages/SingleMovieDetailPage.jsx";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import "./App.css";
 
 function App() {
@@ -19,6 +21,7 @@ function App() {
           <Route path="/movie/:movieId" element={<SingleMovieDetailPage />} />
           <Route path="/search" element={<SearchPage />} />
         </Routes>
+        <Footer/>
       </Router>
     </>
   );
